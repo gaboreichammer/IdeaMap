7bf5b654580cb9b1654fcb7170b65661fc01770a
gaboreichammer <eichgab@gmail.com>
2025-10-26 17:16:43 +0100

    removed H2 database

---
+++ src/main/java/org/ideamap/user/MongoUser.java
@@ -0,0 +1,43 @@
+package org.ideamap.user;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class MongoUser {
+
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    private String username;
+
+    private String hashedPassword;
+
+    public MongoUser(String username, String hashedPassword) {
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+    }
+
+    // Getters and setters for all fields...
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return hashedPassword;
+    }
+
+    public void setPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+}
