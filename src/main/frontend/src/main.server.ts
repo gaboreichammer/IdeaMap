c14326a8b9517d7a421b4a609e16c4eb6db11329
gaboreichammer <eichgab@gmail.com>
2025-11-27 10:48:57 +0100

    Refactor template bindings to track by ID and simplify login loading state

--- src/main/frontend/src/main.server.ts
+++ src/main/frontend/src/main.server.ts
@@ -3,6 +3,6 @@ import { App } from './app/app';
 import { config } from './app/app.config.server';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, {...config, providers: [...config.providers]}, context);
+    bootstrapApplication(App, config, context);
 
 export default bootstrap;
